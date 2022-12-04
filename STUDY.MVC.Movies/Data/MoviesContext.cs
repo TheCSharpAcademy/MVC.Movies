0e682732d32f84390c1f32895bad04d99405a75a
paguirre82 <thecsharpacademy@gmail.com>
2022-12-05 09:07:36 +1100

    Initial

---
+++ STUDY.MVC.Movies/Data/MoviesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using STUDY.MVC.Movies.Models;
+
+namespace STUDY.MVC.Movies.Data;
+
+public class MoviesContext : DbContext
+{
+    public MoviesContext (DbContextOptions<MoviesContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movie { get; set; } = default!;
+}
